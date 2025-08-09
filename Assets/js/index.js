eb5c42b0315925aab161b8c805a7095098536efa
Unizus0983 <leboulangernathalie@gmail.com>
2025-08-09 11:39:00 +0200

    modifications liens et ajout bouton de scroll pour page

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
     const track = document.querySelector('.slider-track');
-    const slides = document.querySelectorAll('.slide');
-    const dots = document.querySelectorAll('.slider-dot');
+    const slides = document.querySelectorAll('.slide'); //TABLE SLIDE
+    const dots = document.querySelectorAll('.slider-dot'); //TABLE DOTS
     let currentIndex = 0;
-    const slideCount = slides.length;
-    let interval;
+    const slideCount = slides.length; //COUNTER FOR SLIDES
+    let interval; //INTERVAL BETWEEN SLIDES
 
     function updateSlider() {
         track.style.transform = `translateX(-${currentIndex * 100}%)`;
@@ -45,11 +45,13 @@ document.addEventListener('DOMContentLoaded', function () {
     startAutoSlide();
 });
 
-// Menu burger
+// VARIABLES
+// variables menu
 const burgerBtn = document.querySelector('.burger-bar');
 const closeBtn = document.querySelector('.close');
 const menuBurger = document.querySelector('.menu-burger');
 
+// MENU BURGER
 if (burgerBtn && closeBtn && menuBurger) {
     burgerBtn.addEventListener('click', () => {
         menuBurger.style.transform = 'translateX(0)';
@@ -59,3 +61,28 @@ if (burgerBtn && closeBtn && menuBurger) {
         menuBurger.style.transform = 'translateX(-200%)';
     });
 }
+
+// BUTTON SCROLL
+document.getElementById('backToTop').addEventListener('click', function () {
+    const start = window.scrollY; //first position begenning in px
+    const end = 0; //position in px
+    const duration = 1000; // duration animation in ms (2s here)
+    const distance = start - end; //distance to do in px to start to end with soustraction
+    let startTime = null; //time distance for beginning
+
+    //timestamp → automatically provided by requestAnimationFrame(method JS), this is the time in milliseconds since the page was opened
+
+    function scrollStep(timestamp) {
+        if (!startTime) startTime = timestamp;
+        const elapsed = timestamp - startTime;
+        const progress = Math.min(elapsed / duration, 1); // linear
+
+        window.scrollTo(0, start - distance * progress);
+
+        if (progress < 1) {
+            requestAnimationFrame(scrollStep);
+        }
+    }
+
+    requestAnimationFrame(scrollStep);
+});
---
+++ README.md
@@ -0,0 +1,2 @@
+[lien vers figma](https://www.figma.com/design/xN4vMpYPqH2074DQ9ANA8b/Projet-Artisan-phographe?node-id=29-4&t=axJwRWDk4jx4UhoA-0)
+#formulaire et admin à terminer je suis en train de faire les bdd :comme correction semaine des vacances je vais quand même faire le reste
